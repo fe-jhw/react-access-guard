0a9c02a1d432af4916797f17b013e4f943718d36
hyunwoo <jhw1234527@gmail.com>
2025-03-23 18:21:36 +0900

    fix: not compatible with react 19

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       formats: ['es', 'cjs']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
       output: {
         globals: {
           react: 'React',
